ae917e6d1a041dd49b68e509044adba8be35c2d8
crypt0 <acvjetan@gmail.com>
2020-09-20 17:48:50 +0200

    Initial code commit.

---
+++ mod.ts
@@ -0,0 +1,20 @@
+import { Application } from "https://deno.land/x/oak@v5.4.0/mod.ts";
+
+const app = new Application();
+const PORT = 8000;
+
+app.use((ctx) => {
+  ctx.response.body = `
+    {___     {__      {_         {__ __        {_       
+    {_ {__   {__     {_ __     {__    {__     {_ __     
+    {__ {__  {__    {_  {__     {__          {_  {__    
+    {__  {__ {__   {__   {__      {__       {__   {__   
+    {__   {_ {__  {______ {__        {__   {______ {__  
+    {__    {_ __ {__       {__ {__    {__ {__       {__ 
+    {__      {__{__         {__  {__ __  {__         {__
+                    Mission Control API`;
+});
+
+app.listen({
+  port: PORT,
+});
